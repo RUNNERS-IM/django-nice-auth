da372c63074d501b44afc42b5cef242dbf874da8
Runners <dev@runners.im>
2024-06-22 16:30:37 +0000

    Bump version: 0.1.6 → 0.1.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="django-nice-auth",
-    version="0.1.6",
+    version="0.1.7",
     packages=find_packages(),
     install_requires=[
         "django>=3.0",
